4cc40fbc0c3657ea0c669f9815f0f7538799dfe4
James Mburu Githire <jamesmburug24@gmail.com>
2022-08-08 13:47:40 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(toDo = "roller-skate"){
+    return `This Saturday, I want to ${toDo}!`;
+}
+
+function mondayWork(toDo = "go to the office"){
+    return `This Monday, I will ${toDo}.`;
+}
+
+function wrapAdjective(parentPar1 = "*"){
+    return (function(par1="special"){
+        return `You are ${parentPar1}${par1}${parentPar1}!`;
+    });
+}
